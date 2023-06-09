19b7db58375d6b0b53b2ec525de20bb98665e8c5
Uttam Makwana <uttamakwana4503@gmail.com>
2023-06-09 19:25:50 +0530

    Add all component into App.jsx and install sass module

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import {
+  Header,
+  About,
+  Skills,
+  Project,
+  Work,
+  Testimonials,
+  Footer,
+} from "./components";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      Hello
+      <Header />
+      <About />
+      <Skills />
+      <Project />
+      <Work />
+      <Testimonials />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
